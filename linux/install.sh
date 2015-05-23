3685fc8a4ae27af124b9c1b726ce15acc0e50e99
Armin Grodon <armin.grodon@googlemail.com>
2015-05-24 00:47:09 +0200

    added submodule mapping

--- linux/install.sh
+++ linux/install.sh
@@ -2,16 +2,16 @@
 
 echo 'Setting up Linux'
 apt > /dev/null 2>&1
-if [[ $? -ne 0 ]]; then
+if [ $? -ne 0 ]; then
     echo 'Cannot run apt'
     return -1
 fi
 
 echo 'Updating system'
-sudo apt-get update > /dev/null
-sudo apt-get -y upgrade > /dev/null
+sudo apt-get update
+sudo apt-get -y upgrade
 
 echo 'Installing basics'
-sudo apt-get install -y ctags git htop tmux vim zsh > /dev/null
+sudo apt-get install -y ctags git htop tmux vim zsh
 
-gconftool-2 --load gnome-terminal.conf.xml
+gconftool-2 --load linux/gnome-terminal.conf.xml
