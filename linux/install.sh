4e9b0bceacc07e4ec2a50a5425b15cc98d450b48
Armin Grodon <armin.grodon@googlemail.com>
2015-05-25 23:23:04 +0200

    fix solarized colors

--- linux/install.sh
+++ linux/install.sh
@@ -14,8 +14,8 @@ sudo apt-get -y upgrade
 echo 'Installing basics'
 sudo apt-get install -y ctags git htop tmux vim zsh
 
-gconftool-2 --load linux/gnome-terminal.conf.xml
-
 if [ ! -d "$HOME/.fonts" ]; then
     mkdir $HOME/.fonts
 fi
+
+gnome-terminal-colors-solarized/install.sh -s dark -p Default
