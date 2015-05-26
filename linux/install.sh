d92ebb519f68f97a90b9fefc806b8674fc962a2c
Armin Grodon <armin.grodon@googlemail.com>
2015-05-26 21:06:52 +0200

    fixed OS-check. Find default gnome profile, rename and set font

--- linux/install.sh
+++ linux/install.sh
@@ -17,4 +17,9 @@ if [ ! -d "$HOME/.fonts" ]; then
     mkdir $HOME/.fonts
 fi
 
+echo 'Setting font and color'
+profile=$(dconf read /org/gnome/terminal/legacy/profiles:/default | tr -d "'")
+dconf write /org/gnome/terminal/legacy/profiles:/:$profile/visible-name "'Default'"
+dconf write /org/gnome/terminal/legacy/profiles:/:$profile/font "'Source Code Pro for Powerline Medium 12'"
+
 linux/gnome-terminal-colors-solarized/install.sh -s dark -p Default
