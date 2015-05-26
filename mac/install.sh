0509830335a9d14e574134b21ec45186fe3d76a2
Armin Grodon <armin.grodon@googlemail.com>
2015-05-26 09:34:58 +0200

    some changes for mac

--- mac/install.sh
+++ mac/install.sh
@@ -2,11 +2,16 @@
 
 echo 'Setting up OSX'
 
-echo 'Installing homebrew'
-ruby -e "$(url -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew --version > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo 'Installing homebrew'
+    ruby -e "$(url -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 echo 'Installing basics'
+brew install caskroom/cask/brew-cask
 brew install git
 brew install tmux
 brew install macvim --override-system-vim
 brew install zsh
+brew cask install iterm2
--- symlinks.sh
+++ symlinks.sh
@@ -7,5 +7,6 @@ linkables=$( find . -maxdepth 2 -name "*.symlink" -exec basename {} \; )
 for file in $linkables ; do
     target="$HOME/.$( basename $file ".symlink" )"
     echo "creating symlink for $file"
+    rm -rf $target > /dev/null 2>&1
     ln -s $DOTFILES/$file $target
 done
