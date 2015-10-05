cf1f4cd26e7fbb4071b5ad26fa82b74be1b3c285
Armin Grodon <armin.grodon@googlemail.com>
2015-10-05 13:45:22 +0200

    installer: removed xclip, added build dependencies.

--- install.sh
+++ install.sh
@@ -17,6 +17,8 @@ sudo apt-add-repository -y ppa:webupd8team/sublime-text-3 > /dev/null 2>&1
 sudo apt-add-repository -y ppa:wiktel/ppa > /dev/null 2>&1
 # todo-indicator
 sudo add-apt-repository -y ppa:nilarimogard/webupd8 > /dev/null 2>&1
+# own ppa
+sudo add-apt-repository -y ppa:x4121/x4121 > /dev/null 2>&1
 # sbt
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list > /dev/null 2>&1
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
@@ -40,11 +42,14 @@ sudo apt-get install -y\
     todo-indicator\
     tree\
     vim-gnome\
-    wmctrl
-    xclip\
-    zsh\
+    wmctrl\
+    zsh
 
 echo 'Installing st'
+sudo apt-get install -y\
+    libxft-dev\
+    libfontconfig1-dev\
+    libxext-dev
 git clone https://github.com/x4121/st $HOME/st_tmp > /dev/null
 pushd $HOME/st_tmp > /dev/null
 make && sudo make install
@@ -61,13 +66,17 @@ rm -rf $HOME/st_tmp
 #rm -rf $HOME/mutt-kz_tmp
 
 #echo 'Installing keepassx'
-# dependencies?
-#git clone https://github.com/x4121/keepassx $HOME/keepassx_tmp > /dev/null
-#mkdir $HOME/keepassx_tmp/build
-#pushd $HOME/keepassx_tmp/build > /dev/null
-#cmake .. && make -j4 && sudo make install
-#popd > /dev/null
-#rm -rf $HOME/keepassx_tmp
+sudo apt-get install -y\
+    libqt4-dev\
+    libgcrypt20-dev\
+    libmicrohttpd-dev\
+    libqjson-dev
+git clone https://github.com/x4121/keepassx $HOME/keepassx_tmp > /dev/null
+mkdir $HOME/keepassx_tmp/build
+pushd $HOME/keepassx_tmp/build > /dev/null
+cmake .. && make -j4 && sudo make install
+popd > /dev/null
+rm -rf $HOME/keepassx_tmp
 
 #echo 'Installing additional software'
 #sudo apt-get install -y\
