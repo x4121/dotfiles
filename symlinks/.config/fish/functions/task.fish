9376224777c6fea89bbf59c389c0fb0773c7ec5d
Armin Grodon <armin.grodon@googlemail.com>
2016-03-15 09:29:52 +0100

    sync taskwarrior after every execution

--- symlinks/.config/fish/functions/task.fish
+++ symlinks/.config/fish/functions/task.fish
@@ -1,15 +1,32 @@
 function task
-    # never synced
-    if not set -q __task_last_sync
-        __task.sync
-    # last sync > 5 min ago
-    else if math (date +%s) - $__task_last_sync " > 300" > /dev/null
+    # initial sync
+    if __task.needs_sync
         __task.sync
     end
-    command task $argv
+
+    __task.exec
+
+    # sync if something changed
+    __task.sync > /dev/null ^ /dev/null
 end
 
 function __task.sync
     set -g __task_last_sync (date +%s)
     command task sync
+end
+
+function __task.exec
+    command task $argv
+end
+
+function __task.needs_sync
+    # never synced
+    if not set -q __task_last_sync
+        return 0
+    # last sync > 5 min ago
+    else if math (date +%s) - $__task_last_sync " > 300" > /dev/null
+        return 0
+    else
+        return 1
+    end
 end
