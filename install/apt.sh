f5705b1ad25cf0500f796cc1227528b11ee7a011
Armin Grodon <me@armingrodon.de>
2017-01-12 09:32:27 +0100

    remove oracle jdk

--- install/apt.sh
+++ install/apt.sh
@@ -15,8 +15,6 @@ SW="apt-transport-tor\
     vim-gnome-py2"
 
 if [[ $DISPLAY != "" ]]; then
-    # gnome-keyring-query
-    #sudo apt-add-repository -y ppa:wiktel/ppa >&- 2>&-
     # owncloud
     wget -q -O - http://download.opensuse.org/repositories/isv:ownCloud:desktop/Ubuntu_16.04/Release.key \
         | sudo apt-key add - >&- 2>&-
@@ -48,7 +46,7 @@ if [[ $DISPLAY != "" ]]; then
         xdotool"
 
     if [[ $DESKTOP_SESSION = gnome ]]; then
-        # arc-theme
+        # arc-theme # remove in 16.10
         wget -q -O - http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_15.04/Release.key \
             | sudo apt-key add - >&- 2>&-
         echo "deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /" \
@@ -74,19 +72,11 @@ if [[ $DISPLAY != "" ]]; then
     fi
 
     if ! [[ -z ${I_DEV+x} ]]; then
-        # oracle java
-        sudo apt-add-repository -y ppa:webupd8team/java >&- 2>&-
-        echo debconf shared/accepted-oracle-license-v1-1 select true | \
-              sudo debconf-set-selections
-        echo debconf shared/accepted-oracle-license-v1-1 seen true | \
-              sudo debconf-set-selections
         # sbt
         sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 \
             --recv 642AC823 >&- 2>&-
         echo "deb https://dl.bintray.com/sbt/debian /" \
             | sudo tee -a /etc/apt/sources.list.d/sbt.list >&- 2>&-
-        # gradle
-        sudo apt-add-repository -y ppa:cwchien/gradle >&- 2>&-
         # docker
         sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 \
             --recv-keys 58118E89F3A912897C070ADBF76221572C52609D - >&- 2>&-
@@ -102,14 +92,12 @@ if [[ $DISPLAY != "" ]]; then
             docker-engine\
             emacs\
             golang\
-            gradle\
             jq\
             knockd\
             libtool\
             maven\
             openjdk-8-jdk\
-            oracle-java9-installer\
-            oracle-java8-set-default\
+            openjdk-9-jdk\
             sbt\
             texlive-full\
             vagrant"
