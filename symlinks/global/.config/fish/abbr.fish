6e2f2fa0df9dc883f4e1e2a9ad19198eb2465944
Armin Grodon <armin@onpage.org>
2017-06-19 16:46:21 +0200

    Fish: fix error in git abbrev

--- symlinks/global/.config/fish/abbr.fish
+++ symlinks/global/.config/fish/abbr.fish
@@ -24,7 +24,7 @@ abbr dp     docker ps #overrides speech-tools
 abbr g      git
 abbr ga     git add
 abbr gb     git branch --verbose
-abbr gba    git branch --verbose --all
+abbr gba    git branch --verbose -a
 abbr gc     git commit -m
 abbr gcl    git clone --recursive
 abbr gco    git checkout
