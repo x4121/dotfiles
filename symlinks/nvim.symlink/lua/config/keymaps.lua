984a5700768627aa47c628afdd1006c553b3fb12
Armin Grodon <me@armingrodon.de>
2024-03-06 21:40:23 +0100

    Nvim: don't use system clipboard by default

--- symlinks/nvim.symlink/lua/config/keymaps.lua
+++ symlinks/nvim.symlink/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from system clipboard" })
--- symlinks/nvim.symlink/lua/config/options.lua
+++ symlinks/nvim.symlink/lua/config/options.lua
@@ -2,3 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 -- vim.opt_global.completeopt = { "menuone", "noinsert", "noselect" }
+
+vim.opt.clipboard = ""
